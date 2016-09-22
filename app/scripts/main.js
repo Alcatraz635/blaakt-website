8a7a9dcba119cab6fd52d79160209a2f8ac680f7
Tyler Schumacher <tylerschumacher635@gmail.com>
2016-09-22 17:30:59 -0400

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,10 @@
 $(document).ready( () => {
-  var wow = new WOW(
+  const wow = new WOW(
    {
      boxClass:     'wow',      // animated element css class (default is wow)
      animateClass: 'animated', // animation css class (default is animated)
      offset:       2,          // distance to the element when triggering the animation (default is 0)
-     mobile:       true,       // trigger animations on mobile devices (default is true)
+     mobile:       false,       // trigger animations on mobile devices (default is true)
      live:         true,       // act on asynchronously loaded content (default is true)
      callback:     function(box) {
        // the callback is fired every time an animation is started
@@ -16,17 +16,17 @@ $(document).ready( () => {
  wow.init();
 
   //Event handlers
-  $(".nav-about").on('click', () => {
+  $('.nav-about').on('click', () => {
     $.smoothScroll({
       scrollTarget: '.content-section-a'
     })
   })
-  $(".nav-music").on('click', () => {
+  $('.nav-music').on('click', () => {
     $.smoothScroll({
       scrollTarget: '.content-section-music'
     })
   })
-  $(".nav-contact").on('click', () => {
+  $('.nav-contact').on('click', () => {
     $.smoothScroll({
       scrollTarget: '.content-section-c'
     })
